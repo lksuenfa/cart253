1a1df430ffb91af8397c87bc1f6fed64fd5da632
Leanne Suen Fa <70963322+lksuenfa@users.noreply.github.com>
2020-09-21 22:10:04 -0400

    E1 : kitty photobomb

--- Exercises/Exercise1/js/script.js
+++ Exercises/Exercise1/js/script.js
@@ -26,6 +26,18 @@ let sun = {
 
 };
 
+let catface = {
+  x: 90,
+  y: 550,
+  w: 350,
+  h : 400,
+  rl: 100,
+  fill: 0
+};
+
+let catpupil = {
+  d : 20
+};
 
 // Description of setup() goes here.
 function setup() {
@@ -49,6 +61,27 @@ sun.y = constrain(sun.y,70,600); //constrain sun movement
 fill(sun.fill, sun.a);
 ellipse(sun.x, sun.y, sun.d);
 
+
+//cat photobomb
+
+//cat face
+fill(catface.fill);
+rect(catface.x,catface.y, catface.w, catface.h, catface.rl);
+
+//catear
+triangle(90,500, 300, 650, 90, 700); //L
+triangle(300,650, 400, 500, 400, 700); //R
+
+//cateye
+fill(255, 200, 0);
+ellipse(180, 650,100); //L
+ellipse(360, 650,100); //R
+
+//catpupil
+fill(0);
+catpupil.d = map(sun.y, 70, 600,20,80); //cat pupil responds to sun position
+ellipse(180, 650,catpupil.d,80); //L
+ellipse(360, 650,catpupil.d,80); //R
 
 
 
