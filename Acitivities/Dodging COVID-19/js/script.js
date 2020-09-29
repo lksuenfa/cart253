401900cd77b74352254536887a602312deeb9e2d
Leanne Suen Fa <70963322+lksuenfa@users.noreply.github.com>
2020-09-29 17:22:23 -0400

    A3 : draw covid19

--- Acitivities/Dodging COVID-19/js/script.js
+++ Acitivities/Dodging COVID-19/js/script.js
@@ -5,7 +5,7 @@ Leanne Suen Fa
 COVID-19, represented by a red circle, will move from the left side of the canvas to the right at a random y position. Each time it reaches the right side, it will reset to the left at a random y position. The user will control their own circle with the mouse position. If the COVID-19 circle touches the user circle, everything stops! In the background we see random static for visual flair and we don’t see the mouse cursor.
 **************************************************/
 
-Let covid19 = {
+let covid19 = {
   x : 0,
   y : 250,
   size : 100,
@@ -24,13 +24,33 @@ function setup() {
 createCanvas (windowWidth, windowHeight);
 
 covid19.y = random(0, height);
-
 covid19.vx = covid19.speed ;
 }
 
+let user = {
+  x : 0,
+  y : 0,
+  size : 100,
+  fill : 255
+};
 
 function draw() {
 
   background(0);
+
+  covid19.x = covid19.x + covid19.vx ;
+  covid19.y = covid19.y + covid19.vy ;
+
+  if (covid19.x > width) {
+    covid19.x = 0;
+    covid19.y = random(0, height);
+  }
+
+  fill(covid19.fill.r, covid19.fill.g, covid19.fill.b );
+  noStroke();
+  ellipse(covid19.x, covid19.y, covid19.size);
+
+
+
 
 }
