f60aa5af1139058b9d230bb35cbdf2d2b43256a4
Leanne Suen Fa <70963322+lksuenfa@users.noreply.github.com>
2020-10-22 19:01:48 -0400

    E4: create special fish and make them move

--- Exercises/Exercise4/js/script.js
+++ Exercises/Exercise4/js/script.js
@@ -1,55 +1,75 @@
-// ( ) Add a user-controlled shape (or image)
-// ( ) Make the user interact with the fish
-// ( ) Change the fish (or whatever) creation
+// (x ) Add a user-controlled shape (or image)
+// (x ) Make the user interact with the fish
+// (x ) Change the fish (or whatever) creation
 // ( ) Add at least two endings
 
 "use strict";
 
 let school = []; // Create an empty array and assign it to the school variable
-let schoolSize = 4; //length of array
+let schoolSize = 50; //length of array
 
 let cupid = {
-  x: 0,
-  y: 0,
+  x: 500,
+  y: 500,
+  vy: 0,
+  vy: 0,
+  speed: 1,
   image: undefined,
 };
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y, r, g, b) {
-  let fish = {
-    x: x,
-    y: y,
-    image: undefined,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2,
-    fill: {
-      r: r,
-      g: 150,
-      b: 150,
-    },
-  };
-  return fish;
-}
+let specialFish1 = {
+  x: 250,
+  y: 250,
+  vy: 0,
+  vy: 0,
+  speed: 1,
+  image: undefined,
+};
+let specialFish2 = {
+  x: 50,
+  y: 50,
+  vy: 0,
+  vy: 0,
+  speed: 1,
+  image: undefined,
+};
 
 function preload() {
-  // fish.image = loadImage("assets/images/fish1.svg");
   cupid.image = loadImage("assets/images/cupid.svg");
+  specialFish1.image = loadImage("assets/images/specialFish1.svg");
+  specialFish2.image = loadImage("assets/images/specialFish2.svg");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // Create four fish, positioned randomly
-
   for (let i = 0; i < schoolSize; i++) {
     school[i] = createFish(random(0, width), random(0, height));
   }
 }
 
-// draw()
-// Moves and displays our fish
+// createFish(x,y)
+// Creates a new JavaScript Object describing a fish and returns it
+function createFish(x, y, r) {
+  let fish = {
+    x: x,
+    y: y,
+    size: 50,
+    vx: 0,
+    vy: 0,
+    speed: 1,
+    fill: {
+      r: r,
+      g: 150,
+      b: 150,
+      a: 150,
+    },
+  };
+  //random fish colour from pink to blue
+  fish.fill.r = random(0, 255);
+  return fish;
+}
+
 function draw() {
   background(230, 236, 237); //grey
 
@@ -67,11 +87,15 @@ function draw() {
     displayFish(school[i]);
   }
 
-  //move cupid
-  imageMode(CENTER);
-  cupid.x = mouseX;
-  cupid.y = mouseY;
-  image(cupid.image, cupid.x, cupid.y, 70, 50);
+  //Cupid movement and display
+  moveCupid();
+  displayCupid();
+
+  //specialFish
+
+  moveFish(specialFish1);
+  moveFish(specialFish2);
+  displaySpecialFish();
 }
 
 // moveFish(fish)
@@ -84,7 +108,26 @@ function moveFish(fish) {
     fish.vy = random(-fish.speed, fish.speed);
   }
 
-  // Move the fish
+  //reset position of fish to random if it reaches the canvas borders
+  if (fish.x === 0 || fish.x === width) {
+    fish.vx = random(0, width);
+  } else if (fish.y === 0 || fish.y === height) {
+    fish.y = random(0, height);
+  }
+
+  //  Move the fish
+  if (mouseX > fish.x) {
+    fish.vx = -fish.speed;
+  } else if (mouseX < fish.x) {
+    fish.vx = fish.speed;
+  }
+
+  if (mouseY > fish.y) {
+    fish.vy = -fish.speed;
+  } else if (mouseY < fish.y) {
+    fish.vy = fish.speed;
+  }
+
   fish.x = fish.x + fish.vx;
   fish.y = fish.y + fish.vy;
 
@@ -97,14 +140,38 @@ function moveFish(fish) {
 // Displays the provided fish on the canvas
 function displayFish(fish) {
   push();
-  fish.fill.r = random(0, 255);
-  // fish.fill.b = random(0, 255);
-  // fish.fill.g = random(0, 255);
-  fill(fish.fill.r, fish.fill.g, fish.fill.b);
+  fill(fish.fill.r, fish.fill.g, fish.fill.b, fish.fill.a);
   noStroke();
   ellipse(fish.x, fish.y, fish.size);
-  // image(fish.image, fish.x, fish.y);
   pop();
+}
+
+function moveCupid() {
+  //move cupid according to cursor position
+  if (mouseX > cupid.x) {
+    cupid.vx = cupid.speed;
+  } else if (mouseX < cupid.x) {
+    cupid.vx = -cupid.speed;
+  }
+
+  if (mouseY > cupid.y) {
+    cupid.vy = cupid.speed;
+  } else if (mouseY < cupid.y) {
+    cupid.vy = -cupid.speed;
+  }
+
+  cupid.x = cupid.x + cupid.vx;
+  cupid.y = cupid.y + cupid.vy;
+}
+
+function displayCupid() {
+  imageMode(CENTER);
+  image(cupid.image, cupid.x, cupid.y, 70, 50);
+}
+
+function displaySpecialFish() {
+  image(specialFish1.image, specialFish1.x, specialFish1.y);
+  image(specialFish2.image, specialFish2.x, specialFish2.y);
 }
 
 function mousePressed() {
